5c4839ef46fd9ade766df6d554512b9fe02be017
Sixela33 <alexis.janko@gmail.com>
2024-04-12 14:23:32 -0300

    Added Loading screen

---
+++ public/src/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/NavBar'
+
+export default function LoadingPage() {
+  return (
+    <>
+        <Navbar/>
+        <div>LoadingPage</div>
+    </>
+  )
+}
--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AlertProvider } from './context/AlertProvider';
+import LoadingPage from './LoadingPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <AlertProvider>
+          <Suspense fallback={<LoadingPage/>}>
           <Routes>
             <Route path="/*" element={<App />}/>
           </Routes>
+          </Suspense>
         </AlertProvider>
       </AuthProvider>
     </BrowserRouter>
