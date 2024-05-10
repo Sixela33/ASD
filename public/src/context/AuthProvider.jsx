c99abb46cf78c20bbe1446467ccd670afaa07c35
Sixela33 <alexis.janko@gmail.com>
2024-05-10 12:55:17 -0300

    changed default trust-device to false

--- public/src/context/AuthProvider.jsx
+++ public/src/context/AuthProvider.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext({});
 // This context stores the users information
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
-    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || true);
+    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || false);
 
     return (
         <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
