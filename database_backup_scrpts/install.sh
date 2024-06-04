e66164da1f739499ab675fb48135430e2d5bc7cf
Sixela33 <alexis.janko@gmail.com>
2024-06-04 15:59:58 -0300

    modifying scripts

--- database_backup_scrpts/install.sh
+++ database_backup_scrpts/install.sh
@@ -3,24 +3,25 @@
 set -eux
 set -o pipefail
 
-apk update
+apt-get update
 
 # install pg_dump
-apk add postgresql-client
+apt-get add postgresql-client
 
 # install gpg
-apk add gnupg
+apt-get add gnupg
 
-apk add aws-cli
+apt-get add aws-cli
 
 # install go-cron
-apk add curl
+apt-get add curl
 curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
 tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
 rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
 mv go-cron /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
-apk del curl
+apt-get del curl
+
 
 # cleanup
-rm -rf /var/cache/apk/*
+rm -rf /var/cache/apt-get/*
