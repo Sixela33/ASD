46772aca21a757cbc9ac46e76ff0770b930f1cc4
Sixela33 <alexis.janko@gmail.com>
2024-06-04 11:17:41 -0300

    added new origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,10 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:8080',
-    'http://195.35.19.195:8080',
-    'http://195.35.19.195:5173',
-    'http://127.0.0.1:5500',
-    'https://ramelax.com.ar',
-    'http://ramelax.com.ar',
-    'http://ramelax.com.ar:80',
+    'http://projects-angelsalazardesign.com',
+    'https://projects-angelsalazardesign.com',
+    'http://www.projects-angelsalazardesign.com',
+    'https://www.projects-angelsalazardesign.com'
 ];
 
 export default allowedOrigins;
