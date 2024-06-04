8d198868757519d62d88bcab08d958baaaf4d383
Sixela33 <alexis.janko@gmail.com>
2024-06-04 16:05:10 -0300

    updating script

--- database_backup_scrpts/install.sh
+++ database_backup_scrpts/install.sh
@@ -3,25 +3,33 @@
 set -eux
 set -o pipefail
 
+# Update package list
 apt-get update
 
-# install pg_dump
-apt-get add postgresql-client
+# Install pg_dump
+apt-get install -y postgresql-client
 
-# install gpg
-apt-get add gnupg
+# Install gpg
+apt-get install -y gnupg
 
-apt-get add aws-cli
+# Install AWS CLI version 2
+apt-get install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -rf awscliv2.zip aws
 
-# install go-cron
-apt-get add curl
-curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
-tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
-rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
+# Install go-cron
+apt-get install -y curl
+if [ -z "${TARGETARCH:-}" ]; then
+  echo "TARGETARCH is not set"
+  exit 1
+fi
+curl -L "https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz" -O
+tar xvf "go-cron_0.0.5_linux_${TARGETARCH}.tar.gz"
+rm "go-cron_0.0.5_linux_${TARGETARCH}.tar.gz"
 mv go-cron /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
-apt-get del curl
 
-
-# cleanup
-rm -rf /var/cache/apt-get/*
+# Cleanup
+rm -rf /var/lib/apt/lists/*
