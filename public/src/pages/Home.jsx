2af9a1cfaa0e33a639030baf93a3e7413af37d93
Sixela33 <alexis.janko@gmail.com>
2024-06-07 19:00:35 -0300

    Fixing main menu visual bug

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
         {buttons.map((button) => (
           (button.url && auth?.decoded?.permissionlevel >= button.minPermissionLevel) && <Link key={button.id} to={button.url} 
-          className="block bg-gray-300 text-black h-[35vh] w-[35vh] flex items-center justify-center square-btn hover:bg-gray-400 focus:outline-none focus:border-none">
+          className="bg-gray-300 text-black h-[35vh] w-[35vh] max-w-[80vw] max-h-[400px] max-w-[400px] flex items-center justify-center hover:bg-gray-400">
             {button.label}
           </Link>
         ))}
