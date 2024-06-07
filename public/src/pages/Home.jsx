925887b5e13e9b81e7c63528ba478e6b801ae1d4
Sixela33 <alexis.janko@gmail.com>
2024-06-07 19:02:47 -0300

    Fix

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
         {buttons.map((button) => (
           (button.url && auth?.decoded?.permissionlevel >= button.minPermissionLevel) && <Link key={button.id} to={button.url} 
-          className="bg-gray-300 text-black h-[35vh] w-[35vh] max-w-[80vw] max-h-[400px] max-w-[400px] flex items-center justify-center hover:bg-gray-400">
+          className="bg-gray-300 text-black h-[35vh] w-[35vh] max-h-[400px] max-w-[400px] flex items-center justify-center hover:bg-gray-400">
             {button.label}
           </Link>
         ))}
