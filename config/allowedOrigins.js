820fb5971c18d917b585db1b993640bfdba54d77
Sixela33 <alexis.janko@gmail.com>
2024-07-08 11:37:10 -0300

    Config

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,6 @@
 const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:8080',
     'http://projects-angelsalazardesign.com',
     'https://projects-angelsalazardesign.com',
     'http://www.projects-angelsalazardesign.com',
