1c5ba630a5758b52548408ed05417250b123c041
Sixela33 <alexis.janko@gmail.com>
2024-08-27 10:34:09 -0300

    Allowing all origins through cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://projects-angelsalazardesign.com',
-    'https://projects-angelsalazardesign.com',
-    'http://www.projects-angelsalazardesign.com',
-    'https://www.projects-angelsalazardesign.com'
+    '*'
 ];
 
 export default allowedOrigins;
